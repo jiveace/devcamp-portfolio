ca66dd8e5aadcd7d2593c7acfc3f34e736893671
Iain A Maxwell <41574824+jiveace@users.noreply.github.com>
2018-11-29 10:59:49 +0000

    Js (#11)
    * Updated portfolio to include attribute for position
    
    * Installed jquery ui, sortable html5 library and css calls
    
    * Integrated initial drag and drop implementation
    
    * Implemented persistent drag and drop functionality on portfolio page
    
    * Added authorization for portfolio drag and drop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_153118) do
+ActiveRecord::Schema.define(version: 2018_11_29_093828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_153118) do
     t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
   create_table "skills", force: :cascade do |t|
