f78a7ca0602f00b0cb294301264b9dbb7d3b8349
Iain A Maxwell <41574824+jiveace@users.noreply.github.com>
2018-11-30 11:56:58 +0000

    Images (#12)
    * Added image to about me page
    
    * Integrated image background for app layout
    
    * Integrated video background along with styles
    
    * Implemented initail image upload with Carrierwave for Portfolios
    
    * Updated file upload forms and refactored placeholder functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,19 @@
 module PortfoliosHelper
+  def default_thumbnail
+    "https://upload.wikimedia.org/wikipedia/en/thumb/7/7e/NJPW-Logo.png/200px-NJPW-Logo.png"
+  end
+
+  def default_main_image
+    "https://cdn.sescoops.com/wp-content/uploads/2018/01/njpw-australia-696x392.jpg"
+  end
+
+  def portfolio_img img, type
+    if img
+      img
+    elsif type == 'thumb'
+      default_thumbnail
+    else
+      default_main_image
+    end
+  end
 end
